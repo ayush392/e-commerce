637276aee2b2a02f9607e68a8b2b5ac9a30d2b0d
ayush392 <ayushgoyal.0004@gmail.com>
2023-11-25 01:07:08 +0530

    Refactor cartController.js

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,28 +1,27 @@
 const User = require("../models/userModel");
-const Product = require("../models/productModel");
+// const Product = require("../models/productModel");
 
 const getCartItems = async (req, res) => {
   try {
     const result = await User.findOne({ _id: req.user._id })
       .select("cart")
       .populate("cart.product");
-    res.status(200).json(result);
+    res.status(200).json(result.cart);
   } catch (error) {
     res.status(400).json(error.message);
   }
 };
 
 const addToCart = async (req, res) => {
-  const { productId, quantity } = req.body;
-  //   console.log(req.body);
-
-  if (!productId || !quantity) {
-    return res.status(404).json("please select product and quantity");
+  const { productId, quantity, size } = req.body;
+  // console.log(req.body);
+  if (!productId || !quantity || !size) {
+    return res.status(404).json("Please provide all the required fields");
   }
   try {
     const result = await User.findByIdAndUpdate(
       { _id: req.user._id },
-      { $push: { cart: { product: productId, quantity } } },
+      { $push: { cart: { product: productId, quantity, size } } },
       { new: true }
     ).populate("cart.product");
 
@@ -33,14 +32,15 @@ const addToCart = async (req, res) => {
 };
 
 const removeFromCart = async (req, res) => {
-  const { productId } = req.body;
+  const { _id } = req.body;
+  // console.log(req.body);
   try {
     const result = await User.findByIdAndUpdate(
       { _id: req.user._id },
-      { $pull: { cart: { product: productId } } },
+      { $pull: { cart: { product: _id } } },
       { new: true }
-    ).populate("cart.product");
-
+    );
+    console.log(result);
     res.json(result);
   } catch (error) {
     res.status(500).json(error.message);
@@ -48,15 +48,16 @@ const removeFromCart = async (req, res) => {
 };
 
 const updateQuantity = async (req, res) => {
-  const { productId, quantity } = req.body;
+  const { _id, quantity, size } = req.body;
+  // console.log(req.body);
   try {
     const result = await User.findOneAndUpdate(
-      { _id: req.user._id, "cart.product": productId },
-      { $set: { "cart.$.quantity": quantity } },
+      { _id: req.user._id, "cart.product": _id },
+      { $set: { "cart.$.quantity": Number(quantity), "cart.$.size": size } },
       { new: true }
-    ).populate("cart.product");
-
-    res.json(result);
+    ).select("cart");
+    console.log(result);
+    res.json(result.cart);
   } catch (error) {
     res.status(500).json(error.message);
   }
