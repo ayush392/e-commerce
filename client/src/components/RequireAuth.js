08d004ffc70bd7491fbdafa4620904f0776f9119
ayush392 <ayushgoyal.0004@gmail.com>
2023-11-25 01:09:54 +0530

    Update token retrieval in RequireAuth component

--- client/src/components/RequireAuth.js
+++ client/src/components/RequireAuth.js
@@ -3,7 +3,9 @@ import { selectToken } from "../redux/auth/authSlice";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 function RequireAuth() {
-  const token = useSelector(selectToken);
+  const token = localStorage.getItem("eCommerceUser")
+    ? JSON.parse(localStorage.getItem("eCommerceUser")).token
+    : null;
   const location = useLocation();
   return token ? (
     <Outlet />
